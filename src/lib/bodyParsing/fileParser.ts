a001469b33432e31b68b2c341ae502d77d15cabb
Stefan Prokop <sprokop1010@seznam.cz>
2020-04-29 08:01:16 +0200

    ✨ Add multer options to file parser

--- src/lib/bodyParsing/fileParser.ts
+++ src/lib/bodyParsing/fileParser.ts
@@ -6,8 +6,8 @@ declare module 'express-serve-static-core' {
     }
 }
 
-export default function fileParser() {
+export default function fileParser(options: multer.Options = {}) {
     const storage = multer.memoryStorage();
-    const fileUpload = multer({ storage });
+    const fileUpload = multer({ ...options, storage });
     return fileUpload.any();
 }
